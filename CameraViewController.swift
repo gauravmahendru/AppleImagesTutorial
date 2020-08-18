a250bd238aab87cfaa7f30561a57be634040c884
Gaurav Mahendru <mahendru.gaurav@outlook.com>
2020-08-18 13:28:12 +1000

    Implemented functionality
    Added functionality in GalleryCollectionViewController and CameraViewcontroller

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -7,24 +7,85 @@
 //
 
 import UIKit
+import CoreData
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        managedObjectContext = appDelegate?.persistantContainer?.viewContext
     }
     
 
     @IBOutlet weak var imageView: UIImageView!
+    var managedObjectContext: NSManagedObjectContext?
     
     @IBAction func takePhoto(_ sender: Any) {
+        let controller = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            controller.sourceType = .camera
+        } else {
+            controller.sourceType = .photoLibrary
+        }
+        
+        controller.allowsEditing = false
+        controller.delegate = self
+        self.present(controller, animated: true, completion: nil)
     }
     
     @IBAction func savePhoto(_ sender: Any) {
+        guard let image = imageView.image else {
+            displayMessage("Cannot save until a photo has been taken!", "Error")
+            return
+        }
+        
+        let date = UInt(Date().timeIntervalSince1970)
+        var data = Data()
+        data = image.jpegData(compressionQuality: 0.8)!
+        
+        let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let url = NSURL(fileURLWithPath: path)
+        
+        if let pathComponent = url.appendingPathComponent("\(date)") {
+            let filePath = pathComponent.path
+            let fileManager = FileManager.default
+            fileManager.createFile(atPath: filePath, contents: data, attributes: nil)
+            
+            let newImage = NSEntityDescription.insertNewObject(forEntityName:
+                "ImageMetaData", into: managedObjectContext!) as! ImageMetaData
+            newImage.filename = "\(date)"
+            
+            do {
+                try self.managedObjectContext?.save()
+                displayMessage("Image has been saved!", "Success!")
+            } catch {
+                displayMessage("Could not save to database", "Error")
+            }
+        }
     }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        displayMessage("There was an error in getting the image", "Error")
+    }
+    
+    func displayMessage(_ message: String,_ title: String) {
+        let alertController = UIAlertController(title: title, message: message,
+                                                preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default,
+                                                handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
--- GalleryCollectionViewController.swift
+++ GalleryCollectionViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
-private let reuseIdentifier = "Cell"
 
-class GalleryCollectionViewController: UICollectionViewController {
+class GalleryCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    private let reuseIdentifier = "imageCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow: CGFloat = 3
+    
+    var imageList = [UIImage]()
+    var imagePathList = [String]()
+    var managedObjectContext: NSManagedObjectContext?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,9 +27,11 @@ class GalleryCollectionViewController: UICollectionViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        managedObjectContext = appDelegate?.persistantContainer?.viewContext
     }
 
     /*
@@ -34,27 +44,96 @@ class GalleryCollectionViewController: UICollectionViewController {
     }
     */
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        do {
+            let imageDataList = try
+                managedObjectContext!.fetch(ImageMetaData.fetchRequest())
+                as [ImageMetaData]
+            
+            if(imageDataList.count > 0) {
+                for data in imageDataList {
+                    let fileName = data.filename!
+                    
+                    if(imagePathList.contains(fileName)) {
+                        print("Image already loaded in. Skipping image")
+                        continue
+                    }
+                    
+                    if let image = loadImageData(fileName: fileName) {
+                        self.imageList.append(image)
+                        self.imagePathList.append(fileName)
+                        self.collectionView!.reloadSections([0])
+                    }
+                }
+            }
+        } catch {
+            print("Unable to fetch list of parties")
+        }
+    }
+    
+    func loadImageData(fileName: String) -> UIImage? {
+        let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let url = NSURL(fileURLWithPath: path)
+        var image: UIImage?
+        if let pathComponent = url.appendingPathComponent(fileName) {
+            let filePath = pathComponent.path
+            let fileManager = FileManager.default
+            let fileData = fileManager.contents(atPath: filePath)
+            image = UIImage(data: fileData!)
+        }
+        
+        return image
+    }
+    
+    
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return imageList.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ImageCollectionViewCell
     
         // Configure the cell
     
+        cell.backgroundColor = UIColor.lightGray
+        cell.imageView.image = imageList[indexPath.row]
+        
         return cell
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout
+        collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath:
+        IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout
+        collectionViewLayout: UICollectionViewLayout, insetForSectionAt
+        section: Int) -> UIEdgeInsets {
+        
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout
+        collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt
+        section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
     // MARK: UICollectionViewDelegate
 
     /*
